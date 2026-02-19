8f88bc0bfa2c22df46bbb3df5f85498ebd4226b9
Phan Son <minikie456@gmail.com>
2026-02-20 00:20:53 +0700

    [fe] Adjust router, routes and route to App.jsx; Add structure for MainPage.jsx

--- frontend-website/src/App.jsx
+++ frontend-website/src/App.jsx
@@ -1,11 +1,26 @@
 import { useState } from 'react'
 import './App.css'
 import { AppCtx } from './context/AppContext'
-import VietnamMap from './components/VietnamMap'
+import MainPage from './pages/main/MainPage'
+import LoginPage from './pages/login/LoginPage'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+function AppContent() {
+  return (
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
+  )
+}
 
 function App() {
   return (
-    <VietnamMap />
+    <Router>
+      <AppCtx>
+        <AppContent />
+      </AppCtx>
+    </Router>
   )
 }
 
--- frontend-website/src/pages/main/MainPage.jsx
+++ frontend-website/src/pages/main/MainPage.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import {React, useContext, useState, useEffect} from "react";
+import VietnamMap from "../../components/VietnamMap";
 import "./MainPage.css";
+import { AppCtx } from "../../context/AppContext";
 
-export default function MainPage(){
+export default function MainPage() {
+
+    const jwt = useContext(AppCtx).jwt;
+    const [persons, setPersons] = useState([]);
+    const role = useContext(AppCtx).role;
+
     return (
         <div id="main-page">
-            
+            <div id="container">
+                <div className="layers" id="map-layer">
+                    <VietnamMap />
+                </div>
+                <div className="layers" id="tool-layer">
+
+                </div>
+            </div>
         </div>
     )
 }
+
